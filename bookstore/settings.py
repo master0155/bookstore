7f141786ebf32a125966787ee74468371d86b536
Gabriel <gabrielsilvalima1994@gmail.com>
2026-03-23 16:32:33 -0300

    Ajusta redirect de login do frontend

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -125,6 +125,10 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
