d15643bb2f3a3e37f76ee3612cf8094cf715fd82
Gabriel <gabrielsilvalima1994@gmail.com>
2026-03-23 15:59:31 -0300

    .

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from tweet.models import Tweet
+from tweet.models import Tweet, Like, Comment
 
 
 @admin.register(Tweet)
@@ -8,3 +8,18 @@ class TweetAdmin(admin.ModelAdmin):
     list_filter = ['created_at', 'author']
     search_fields = ['content', 'author__username']
     readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'tweet', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['user__username']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'tweet', 'content', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['author__username', 'content']
+    readonly_fields = ['created_at']
--- tweet/viewsets.py
+++ tweet/viewsets.py
@@ -1,16 +1,50 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from tweet.models import Tweet
-from tweet.serializers import TweetSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
+from tweet.models import Tweet, Like, Comment
+from tweet.serializers import TweetSerializer, CommentSerializer, LikeSerializer
 
 
 class TweetViewSet(ModelViewSet):
-    queryset = Tweet.objects.all()
     serializer_class = TweetSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
     def get_queryset(self):
         return Tweet.objects.all().order_by('-created_at')
-    
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def like(self, request, pk=None):
+        tweet = self.get_object()
+        like, created = Like.objects.get_or_create(user=request.user, tweet=tweet)
+        if not created:
+            return Response({'detail': 'Você já curtiu este tweet.'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'detail': 'Tweet curtido!'}, status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def unlike(self, request, pk=None):
+        tweet = self.get_object()
+        deleted, _ = Like.objects.filter(user=request.user, tweet=tweet).delete()
+        if not deleted:
+            return Response({'detail': 'Você não curtiu este tweet.'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'detail': 'Curtida removida!'}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def comment(self, request, pk=None):
+        tweet = self.get_object()
+        serializer = CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user, tweet=tweet)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=['get'])
+    def comments(self, request, pk=None):
+        tweet = self.get_object()
+        comments = tweet.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
